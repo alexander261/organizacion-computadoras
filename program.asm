4daba9b2317d8b68c07ffd9a6c679829124e314e
alexander261 <kevin.pancho@uabc.edu.mx>
2025-03-12 05:18:33 +0000

    comandos

--- program.asm
+++ program.asm
@@ -1,1 +1,16 @@
+section .data
+    msg db "Hola, mundo!", 0Ah  
 
+section .text
+    global _start
+
+_start:
+    mov edx, 13       
+    mov ecx, msg      
+    mov ebx, 1        
+    mov eax, 4        
+    int 0x80          
+
+    mov eax, 1        
+    xor ebx, ebx      
+    int 0x80          
